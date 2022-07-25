c3617b7314108440f989b63788080e9aee454453
xianii <xianyi.xia@outlook.com>
2022-07-25 14:23:14 +0800

    update

--- registChanHandler.go
+++ registChanHandler.go
@@ -18,6 +18,13 @@ var registRules []handlerRule = []handlerRule{
 		str += "还没有有效命令"
 		f(str)
 	}},
+	{`^\s*HelloWorld\s*$`, func(ctxCommon *khl.EventDataGeneral, s []string, f func(string) string) {
+		if _, ok := registArray[ctxCommon.AuthorID]; !ok {
+			f("已在处置流程中，请尽快完成操作。")
+			return
+		}
+		registReq(guildId, ctxCommon.AuthorID, f, localSession)
+	}},
 }
 
 func registChanHandler(ctxCommon *khl.EventDataGeneral) {
@@ -41,8 +48,9 @@ func registChanHandler(ctxCommon *khl.EventDataGeneral) {
 }
 
 type msdIdCode struct {
-	code  string
-	msgId string
+	code    string
+	msgId   string
+	guildId string
 }
 
 var registArray map[string]msdIdCode = make(map[string]msdIdCode)
@@ -58,31 +66,34 @@ var reactionArray = []string{
 	"7️⃣",
 }
 
-func registJoinHandler(ctx *khl.GuildMemberAddContext) {
-	send := func(words string) string {
-		resp, _ := sendMarkdown(ctx.Common.TargetID, words)
-		return resp.MsgID
-	}
-	registArray[ctx.Extra.UserID] = msdIdCode{code: reactionArray[rand.Intn(7)]}
-	id := send("检测到未识别对象 (met)" + ctx.Extra.UserID + "(met) ，请在`100`秒内点击本条消息下方的图标`" +
-		registArray[ctx.Extra.UserID].code + "`完成消毒处置，超时或者操作错误将被强制弹出。")
-	registArray[ctx.Extra.UserID] = msdIdCode{code: registArray[ctx.Extra.UserID].code, msgId: id}
+func registReq(guildId string, userId string, send func(string) string, session *khl.Session) {
+	code := reactionArray[rand.Intn(7)]
+	id := send("检测到未识别对象 (met)" + userId + "(met) ，请在`100`秒内点击本条消息下方的图标`" +
+		code + "`完成消毒处置，超时或者操作错误将被强制弹出。")
+	registArray[userId] = msdIdCode{code: registArray[userId].code, msgId: id, guildId: guildId}
 	for _, v := range reactionArray {
-		ctx.Session.MessageAddReaction(id, v)
+		session.MessageAddReaction(id, v)
 	}
-	// TODO:
-	// 100秒后，如果registArray[ctx.Extra.UserID]仍然存在，则踢出用户，并删除条目
-
+	// DONE:
+	// 100秒后，如果registArray[userId]仍然存在，则踢出用户，并删除条目
 	go func() {
 		<-time.After(time.Second * time.Duration(100))
-		if _, ok := registArray[ctx.Extra.UserID]; !ok {
+		if _, ok := registArray[userId]; !ok {
 			return
 		}
-		send("(met)" + ctx.Extra.UserID + "(met) 超时未完成处置，已被强制弹出")
-		ctx.Session.GuildKickout(guildId, ctx.Extra.UserID)
-		ctx.Session.MessageDelete(registArray[ctx.Extra.UserID].msgId)
-		delete(registArray, ctx.Extra.UserID)
+		send("(met)" + userId + "(met) 超时未完成处置，已被强制弹出")
+		session.GuildKickout(registArray[userId].guildId, userId)
+		session.MessageDelete(registArray[userId].msgId)
+		delete(registArray, userId)
 	}()
+}
+
+func registJoinHandler(ctx *khl.GuildMemberAddContext) {
+	send := func(words string) string {
+		resp, _ := sendMarkdown(registChannel, words)
+		return resp.MsgID
+	}
+	registReq(ctx.Common.TargetID, ctx.Extra.UserID, send, ctx.Session)
 }
 
 func registReactionHandler(ctx *khl.ReactionAddContext) {
@@ -106,13 +117,13 @@ func registReactionHandler(ctx *khl.ReactionAddContext) {
 		// 如果正确，则赋予basicPrivilege权限，否则将移除用户
 		if ctx.Extra.Emoji.ID == registArray[ctx.Extra.UserID].code {
 			// DONE:
-			ctx.Session.GuildRoleGrant(guildId, ctx.Extra.UserID, basicPrivilege)
+			ctx.Session.GuildRoleGrant(registArray[ctx.Extra.UserID].guildId, ctx.Extra.UserID, basicPrivilege)
 			reply("(met)" + ctx.Extra.UserID + "(met) 已成功执行消毒处置程序，现已对其开放大厅权限")
 		} else {
 			// DONE:
 			go func() {
 				<-time.After(time.Second * time.Duration(10))
-				ctx.Session.GuildKickout(guildId, ctx.Extra.UserID)
+				ctx.Session.GuildKickout(registArray[ctx.Extra.UserID].guildId, ctx.Extra.UserID)
 			}()
 			reply("(met)" + ctx.Extra.UserID + "(met) 被识别为入侵者，将在`10`s后强制弹出")
 		}
